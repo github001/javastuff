7ff9feb129bd6b8962ee60ef14c4600d793293bd
github001 <jakehutson@gmail.com>
2013-07-26 01:40:34 -0400

    second commit

---
+++ booleans.java
@@ -0,0 +1,23 @@
+public class booleans // why will Y not initalize but Z will?
+{
+
+public static void main(String arg[])
+{
+ int X;
+ int Y;
+ int Z;
+ 
+ Y = 9;
+ 
+ X = 20;
+ 
+ if (X == 20)
+  Y = 0;
+ 	
+ if (Y == 0);
+   Z = X;
+	
+	System.out.println(Y);
+	System.out.println(Z);
+	}
+	}
