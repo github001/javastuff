588761bd03b0cc3ce63a7c4dd9040997e8dfa359
github001 <jakehutson@gmail.com>
2013-07-25 22:28:57 -0400

    first commit

---
+++ stars.java
@@ -0,0 +1,17 @@
+public class stars{
+ public static void main(String args[])
+  {
+   String string1 ="*";
+	String string2 ="***";
+	String string3 ="*****";
+	String string4 ="*******";
+	 
+	 System.out.println("   " + string1);
+	 System.out.println("  " + string2);
+	 System.out.println(" " + string3);
+	 System.out.println(string4);
+	 System.out.println(" " + string3);
+	 System.out.println("  " + string2);
+	 System.out.println("   " +string1);
+	 }
+	 }
